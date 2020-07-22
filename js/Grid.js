e5ef5679c789f315137c5b0b480cc56feea84dc4
Patrick Dang <43787923+PDang176@users.noreply.github.com>
2020-07-22 00:07:25 -0700

    Started the basis (grid, nodes)

---
+++ js/Grid.js
@@ -0,0 +1,19 @@
+class Grid{
+    constructor(){
+        this.nodes = new Array(25).fill().map(n => new Node());
+        this.initializeNodes();
+    }
+
+    initializeNodes(){
+        let n = 0;
+        for(let i = 0; i < 5; i++){
+            for(let j = 0; j < 5; j++){
+                this.nodes[n].x = j;
+                this.nodes[n].y = i;
+                n++;
+            }
+        }
+        this.nodes[0].is_Start = true;
+        this.nodes[24].is_End = true;
+    }
+}
---
+++ js/Node.js
@@ -0,0 +1,11 @@
+class Node{
+    constructor(){
+        this.x = null;
+        this.y = null;
+        this.is_Start = false;
+        this.is_End = false;
+        this.g = null;
+        this.h = null;
+        this.f = null;
+    }
+}
